65b90567503c4a94ad38909601d10f03cbac0fc8
sirse <artiom.tretjakovas2@gmail.com>
2023-11-04 14:24:35 +0200

    Added basic loop statement
    https://docs.oracle.com/en/database/oracle/oracle-database/19/lnpls/basic-LOOP-statement.html#GUID-99AC48AC-D868-43C4-9E4D-6A7671942A39

---
+++ src/main/java/org/example/plsql/ast/LoopStatement.java
@@ -0,0 +1,33 @@
+package org.example.plsql.ast;
+
+import java.util.List;
+
+public class LoopStatement extends Statement {
+
+    private List<Statement> statements;
+
+    public LoopStatement(List<Statement> statements) {
+        this.statements = statements;
+    }
+
+    @Override
+    public boolean areEqual(Node node) {
+        if (!(node instanceof LoopStatement)) {
+            return false;
+        }
+
+        LoopStatement other = (LoopStatement) node;
+
+        if (other.statements.size() != this.statements.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < this.statements.size(); ++i) {
+            if (!this.statements.get(i).areEqual(other.statements.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/resources/parser_tests/5_loop_statement/in.pls
@@ -0,0 +1,23 @@
+/*
+ * Program for checking LOOP statement parsing
+ */
+BEGIN
+    -- Basic loop
+    LOOP
+        x := 5;
+    END LOOP;
+
+    -- Multiple statements in loop
+    LOOP
+        x := 5;
+        x := x * 5 ** 6 - 3;
+    END LOOP;
+
+    -- Nested loop
+    LOOP
+        i := 1;
+        LOOP
+            i := i + 1;
+        END LOOP;
+    END LOOP;
+END;
