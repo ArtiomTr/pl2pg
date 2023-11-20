85e94432a2309694bbef773e191a91d13e85df6b
sirse <artiom.tretjakovas2@gmail.com>
2023-11-20 23:14:06 +0200

    Replace AstCompare.java comparison utilities with assertj (usingRecursiveComparison)

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,6 +1,13 @@
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSerializer;
+import com.google.gson.JsonObject;
 import org.example.plsql.ast.*;
 import org.example.plsql.main.ParseException;
 import org.example.plsql.main.Parser;
@@ -11,10 +18,15 @@ import java.io.*;
 import java.lang.reflect.Type;
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.*;
+
 
 public class ParserTest {
 
-    private static final Type PROGRAM_TYPE = new TypeToken<Program>() {
+    private static final Type PROGRAM_TYPE = new com.google.gson.reflect.TypeToken<Program>() {
     }.getType();
 
     private static class SerdePreservingClass implements JsonSerializer<Object>, JsonDeserializer<Object> {
@@ -35,7 +47,7 @@ public class ParserTest {
         @Override
         public JsonElement serialize(Object obj, Type type, JsonSerializationContext jsonSerializationContext) {
             JsonElement jsonElement = jsonSerializationContext.serialize(obj, obj.getClass());
-            jsonElement.getAsJsonObject().addProperty(CLASS_META_KEY, obj.getClass().getCanonicalName());
+            jsonElement.getAsJsonObject().addProperty(CLASS_META_KEY, obj.getClass().getName());
             return jsonElement;
         }
     }
@@ -45,13 +57,16 @@ public class ParserTest {
         URL url = this.getClass().getResource("/parser_tests");
         Assertions.assertNotNull(url);
         File testsFolder = new File(url.getFile());
-        String[] tests = testsFolder.list();
+        String[] tests = Arrays.stream(Objects.requireNonNull(testsFolder.list())).sorted().toArray(String[]::new);
         Assertions.assertNotNull(tests);
         Assertions.assertTrue(tests.length > 0);
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(Statement.class, new SerdePreservingClass())
                 .registerTypeAdapter(Expression.class, new SerdePreservingClass())
                 .registerTypeAdapter(Cursor.class, new SerdePreservingClass())
+                .registerTypeAdapter(Declaration.class, new SerdePreservingClass())
+                .registerTypeAdapter(TypeToken.class, new SerdePreservingClass())
+                .registerTypeAdapter(SubtypeDefinition.Constraint.class, new SerdePreservingClass())
                 .create();
 
         for (String test : tests) {
@@ -100,7 +115,7 @@ public class ParserTest {
             if (expected == null) {
                 Assertions.assertEquals(expected, received, "Expected parser to fail, but instead, it returned " + gson.toJson(received, PROGRAM_TYPE));
             } else {
-                Assertions.assertTrue(AstCompare.areEqual(expected, received), "Expected parser to return " + gson.toJson(expected, PROGRAM_TYPE) + ", but instead it returned " + gson.toJson(received, PROGRAM_TYPE));
+                assertThat(received).usingRecursiveComparison().isEqualTo(expected);
             }
         }
     }
